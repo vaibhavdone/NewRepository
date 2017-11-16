7afe62c11dbf13904aa734268f2ec3932c1cdd1e
Saikat Das <dassaikat93@yahoo.com>
2017-11-16 11:33:52 +0530

    added MyAdd.java ; a method to add 2 int number.

---
+++ MyAdd.java
@@ -0,0 +1,25 @@
+class MyAdd
+
+{
+  
+public int addTwoNumber(int a,int b)
+ 
+ {
+   
+ return a+b;
+  
+}
+  
+public static void main(String args[])
+ 
+ {
+   
+	 MyAdd ob=new MyAdd();
+    
+	int c=ob.addTwoNumber(10,20);
+   
+	 System.out.println("Addition of 10 and 20 is- "+c);
+ 
+ }
+
+}
